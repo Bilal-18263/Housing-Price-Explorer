1c4921d6113433ace6af7e39d738e25543ea900d
Bilal <bilal833502@gmail.com>
2025-12-11 14:02:11 +0500

    initial project setup

---
+++ usahousing.py
@@ -0,0 +1,15 @@
+"""
+Housing Price Explorer + Price Suggestion Utility
+BSAI-3A - Programming for AI Project
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
+plt.style.use('seaborn-v0_8-darkgrid')
+sns.set_palette("husl")
+print("Project initialized with required imports")
